77322deadbc046edb56a9d03b4858730e967b020
Alyona Brytvina <alyona.ivanova.kh@gmail.com>
2021-09-11 18:23:34 +0300

    change with bem and sass

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 
-let filledHearts = document.querySelectorAll('.logo_heart');
+let filledHearts = document.querySelectorAll('.footer__heart-img');
 
 let imgHeartUrl = "https://res.cloudinary.com/intellectfox/image/upload/v1629752957/fe/foxgram/posts/like_xw2apm.svg";
 let imgFilledHeartUrl = "https://res.cloudinary.com/intellectfox/image/upload/v1629752958/fe/foxgram/posts/like-filled_zurlii.svg";
@@ -7,8 +7,7 @@ let imgFilledHeartUrl = "https://res.cloudinary.com/intellectfox/image/upload/v1
 
 filledHearts.forEach((iconImgEl) =>{
     iconImgEl.addEventListener('click', (event) => {
-    const likesCounter = event.target.parentElement.querySelector('.text_post_likes');
-    console.log(event.target.parentElement)
+    const likesCounter = event.target.parentElement.querySelector('.footer__likes-number');
     const counter = +likesCounter.textContent;
 
         if(event.target.src == imgFilledHeartUrl) {
@@ -20,5 +19,7 @@ filledHearts.forEach((iconImgEl) =>{
         }
     });
 })
+
+
 
 
